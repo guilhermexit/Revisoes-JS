659f1bb3b8aa8b9fcde27e42e23cd2fd0b348ac7
guilhermexit <rm93150@fiap.com.br>
2022-09-16 08:05:30 -0300

    First Commit

---
+++ exercicios/index.js
@@ -0,0 +1,21 @@
+var a = document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseover', entrar)
+a.addEventListener('mouseout', sair)
+
+    function clicar()
+    {
+        var a = document.getElementById('area')
+        a.innerText = 'Clicou'
+        a.style.background = 'red'
+    }
+    function entrar(){
+        var a = document.getElementById('area')
+        a.innerText = 'Entrou!'
+        a.style.background = 'blue'
+    }
+    function sair(){
+        var a = document.getElementById('area')
+        a.innerText = 'Saiu!'
+        a.style.background = 'green'
+    }
