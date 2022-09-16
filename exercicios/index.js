65c744f8d1f9945e368a3fc349e951c3ca41be5d
guilhermexit <rm93150@fiap.com.br>
2022-09-16 08:10:01 -0300

    blablabla

--- exercicios/index.js
+++ exercicios/index.js
@@ -2,7 +2,7 @@ var a = document.getElementById('area')
 a.addEventListener('click', clicar)
 a.addEventListener('mouseover', entrar)
 a.addEventListener('mouseout', sair)
-
+ /* Função clicar */
     function clicar()
     {
         var a = document.getElementById('area')
